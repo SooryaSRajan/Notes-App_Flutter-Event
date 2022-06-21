926bbc34bf27bd9f93b9430ae76a20c7186aeba2
sooryasrajan <soorya.s27@gmail.com>
2022-06-21 13:32:02 +0530

    Added comments in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    //Material App Widget, creates a root widget to support Material Design Widgets
     return MaterialApp(
-      title: 'Flutter Demo',
+      //Theme data, to configure theme
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      //home, holds a Widget/Page (the first page you want to see on a screen)
       home: const GreetingsPage(),
     );
   }
